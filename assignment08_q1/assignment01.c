e3ed16e7d5ac52a925e50df12567a4a2fb332394
error037 <fntushh404@gmail.com>
2024-05-13 17:00:38 +0530

    d8_q1

---
+++ assignment08_q1/assignment01.c
@@ -0,0 +1,65 @@
+#include<unistd.h>
+#include<stdio.h>
+
+int main() {
+	int ret1, ret2, ret3, ret4, ret5, i;
+		ret1 = fork();
+		if(ret1 == 0) {
+			for(i=1; i<=3; i++) {
+					printf("child 1: %d\n", i);
+					printf("child 1: pid=%d\n", getpid());
+					printf("child 1: parent pid=%d\n", getppid());
+					sleep(1);
+					}
+					_exit(5);
+			}
+		ret2 = fork();
+		if(ret2 == 0) {
+			for(i=1; i<=3; i++) {
+					printf("child 2: %d\n", i);
+
+					printf("child 2: pid=%d\n", getpid());
+					printf("child 2: parent pid=%d\n", getppid());
+					sleep(1);
+					}
+					_exit(5);
+			}
+		ret3 = fork();
+		if(ret3 == 0) {
+			for(i=1; i<=3; i++) {
+					printf("child 3: %d\n", i);
+
+					printf("child 3: pid=%d\n", getpid());
+					printf("child 3: parent pid=%d\n", getppid());
+					sleep(1);
+					}
+					_exit(5);
+			}
+		ret4 = fork();
+		if(ret4 == 0) {
+			for(i=1; i<=3; i++) {
+					printf("child 4: %d\n", i);
+					printf("child 4: pid=%d\n", getpid());
+					printf("child 4: parent pid=%d\n", getppid());
+					sleep(1);
+					}
+					_exit(5);
+			}
+		ret5 = fork();
+		if(ret5 == 0) {
+			for(i=1; i<=3; i++) {
+					printf("child 5: %d\n", i);
+					printf("child 5: pid=%d\n", getpid());
+					printf("child 5: parent pid=%d\n", getppid());
+					sleep(1);
+					}
+					_exit(5);
+			}
+		else {
+			for(i=1; i<=3; i++) {
+					printf("parent: %d\n", i);
+					sleep(1);
+					}
+			}
+			return 0;
+			}
